ba5f9add7e50c0a08bbc0d3ee4c42a7de9cd0033
taufiq <ataufiq665@gmail.com>
2017-12-24 01:44:22 +0700

    implement TextInput

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default class App extends React.Component {
-  state={
-    tasks:[
-      "Wash",
-      "Clean",
-      "learn"
-    ]
+  state = {
+    tasks:["Wash", "Clean", "learn", "Add Tasks."]
+  };
+
+  handleTextCreation = task => {
+    alert(task);
   };
 
   render() {
     return (
       <View justifyContent="space-around" height="100%">
-          <View flex={1} backgroundColor="pink">
-            <Text>Input</Text>
-          </View>
+          <TaskInput onTaskCreation={this.handleTextCreation}/>
           <TaskList >
             {this.state.tasks.map((task, index) =>
               <Task key={index} description={task}/>
@@ -26,14 +24,49 @@ export default class App extends React.Component {
   }
 }
 
+class TaskInput extends React.Component {
+
+  state = {
+    text: ""
+  };
+
+  handleTextChange = text => {
+    this.setState({text});
+  };
+
+  handleSubmit = () => {
+    this.props.onTaskCreation(this.state.text);
+    this.setState({ text: "" })
+  }
+
+  render() {
+    return (
+    <View flex={1} backgroundColor="pink" justifyContent="center" padding={40}>
+      <TextInput style={styles.inputName}
+      onChangeText={this.handleTextChange}
+      onSubmitEditing={this.handleSubmit}
+      value={this.state.text}
+      />
+    </View>
+  );
+  }
+}
+
 const TaskList = props =>
-<View flex={3} backgroundColor="limegreen" alignItems="center">
+<View flex={3} alignItems="center">
   {props.children}
 </View>;
 
 const Task = props =>
-<View width={300} backgroundColor="white" margin={20}>
-  <Text>
+<View width={300} backgroundColor="#eee" margin={10} padding={10}>
+  <Text fontSize={20}>
     {props.description}
   </Text>
 </View>;
+
+const styles = StyleSheet.create({
+  inputName:{
+    backgroundColor: 'white',
+    height: 30
+  }
+});
